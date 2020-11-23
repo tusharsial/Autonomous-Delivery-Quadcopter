a631b8cf5d38eb37a9e801869ae2ba3c4727808e
Tushar Sial <piyushsial@gmail.com>
2020-11-24 00:31:13 +0530

    Updating final QR scanner script for task 2

--- qr_detect.py
+++ qr_detect.py
@@ -2,32 +2,44 @@
 
 
 '''
-This is a boiler plate script that contains an example on how to subscribe a rostopic containing camera frames 
+This is a boiler plate script that contains an example on how to subscribe a rostopic containing camera frames
 and store it into an OpenCV image to use it further for image processing tasks.
 Use this code snippet in your code or you can also continue adding your code in the same file
 '''
 
-
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
 import rospy
+from pyzbar import pyzbar
+import re
+from std_msgs.msg import String
 
 class image_proc():
 
 	# Initialise everything
 	def __init__(self):
-		rospy.init_node('barcode_test') #Initialise rosnode 
+		rospy.init_node('barcode_test') #Initialise rosnode
 		self.image_sub = rospy.Subscriber("/edrone/camera/image_raw", Image, self.image_callback) #Subscribing to the camera topic
+		self.dest_pub = rospy.Publisher('/dest_cord',String,queue_size=1)
 		self.img = np.empty([]) # This will contain your image frame from camera
 		self.bridge = CvBridge()
-
+         
 
 	# Callback function of amera topic
 	def image_callback(self, data):
 		try:
-			self.img = self.bridge.imgmsg_to_cv2(data, "bgr8") # Converting the image to OpenCV standard image
+			#self.img = self.bridge.imgmsg_to_cv2(data, "bgr8") # Converting the image to OpenCV standard image
+			image = self.bridge.imgmsg_to_cv2(data, "bgr8") # Converting the image to OpenCV standard image
+			barcodes = pyzbar.decode(image)
+			for barcode in barcodes:
+				barcodeData = barcode.data.decode("utf-8")
+				setpoint=barcodeData
+				print(setpoint, type(setpoint))
+				self.dest_pub.publish(setpoint)
+				 
 		except CvBridgeError as e:
 			print(e)
 			return
